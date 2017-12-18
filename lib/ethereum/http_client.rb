b4bca0bc9af128016efa42adaee0ea106ab54def
shao1555 <shao@1555.info>
2017-12-18 15:33:48 +0900

    add batch support for http client

--- lib/ethereum/http_client.rb
+++ lib/ethereum/http_client.rb
@@ -28,7 +28,12 @@ module Ethereum
     end
 
     def send_batch(batch)
-      raise NotImplementedError
+      result = send_single(batch.to_json)
+      result = JSON.parse(result)
+
+      # Make sure the order is the same as it was when batching calls
+      # See 6 Batch here http://www.jsonrpc.org/specification
+      return result.sort_by! { |c| c['id'] }  
     end
   end
 
