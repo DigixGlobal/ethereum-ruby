f59dd61ce6b46d12f077f7dc4b61143aa42d8029
shao1555 <shao@1555.info>
2018-03-30 15:16:34 +0900

    better exception

--- lib/ethereum/http_client.rb
+++ lib/ethereum/http_client.rb
@@ -2,6 +2,8 @@ require 'net/http'
 module Ethereum
   class HttpClient < Client
     attr_accessor :host, :port, :uri, :ssl
+    
+    class ConnectionError < StandardError; end
 
     def initialize(host, port, ssl = false, log = false)
       super(log)
@@ -23,8 +25,22 @@ module Ethereum
       header = {'Content-Type' => 'application/json'}
       request = ::Net::HTTP::Post.new(uri, header)
       request.body = payload
-      response = http.request(request)
-      return response.body
+      begin
+        response = http.request(request)
+      rescue EOFError, Errno::ECONNRESET, SocketError, Net::OpenTimeout => ex
+        raise ConnectionError.new("[#{ex.class}] #{ex.message}")
+      end
+      if response.class == Net::HTTPOK
+        if @use_cookie
+          set_cookie_headers = response.get_fields('set-cookie')
+          if set_cookie_headers.present?
+            @cookie = set_cookie_headers.map{|str| str.split('; ').first}.join('; ')
+          end
+        end
+        return response.body
+      else
+        raise ConnectionError.new("[#{response.class}] #{response.body}")
+      end
     end
 
     def send_batch(batch)
